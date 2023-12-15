36a8e11893eb53896c54596b77e2073e01f9c198
oleksandrsenkiv <oleksandrsenkiv93@gmail.com>
2023-12-15 10:20:17 +0200

    added lab 7

--- src/main.py
+++ src/main.py
@@ -1,42 +1,54 @@
-
-class Node:
-    def __init__(self):
-        self.is_end_of_string = False
-        self.children = {}
+from collections import defaultdict
 
 
-class Trie:
-    def __init__(self):
-        self.root = Node()
+def find_longest_sequence(word_list):
+    adj_list = defaultdict(list)
+    word_set = set(word_list)
+    longest_sequence = 1
+    visited_adj_list = {}
 
-    def add_pattern(self, pattern):
-        node = self.root
-        for char in pattern:
-            if char not in node.children:
-                node.children[char] = Node()
-            node = node.children[char]
-        node.is_end_of_string = True
+    for word in word_list:
+        for i in range(len(word)):
+            new_word = word[:i] + word[i+1:]
+            if new_word != word and new_word in word_set:
+                adj_list[word].append(new_word)
 
-    def find_word(self, word):
-        node = self.root
-        for char in word:
-            if char in node.children:
-                node = node.children[char]
+    for word in word_list:
+        visited = set()
+        queue = [word]
+        visited.add(word)
+        sequence_index = 0
+        vertex_list = []
+        while queue:
+            vertex = queue.pop(0)
+            vertex_list.append(vertex)
+            if vertex not in visited_adj_list:
+                for neighbor in adj_list[vertex]:
+                    if neighbor not in visited:
+                        queue.append(neighbor)
+                        visited.add(neighbor)
+                        sequence_index += 1
             else:
-                return node.is_end_of_string
-        return node.is_end_of_string
+                sequence_index += visited_adj_list[vertex]
+                break
+        if sequence_index > longest_sequence:
+            longest_sequence = sequence_index
+        if word not in visited_adj_list:
+            visited_adj_list[word] = sequence_index
+            vertex_list = vertex_list[1:]
+            if sequence_index != 0:
+                for vertex in vertex_list:
+                    sequence_index -= 1
+                    visited_adj_list[vertex] = sequence_index
+    return longest_sequence
 
-    def find_prefix(self, pref):
-        node = self.root
-        for char in pref:
-            if char not in node.children:
-                return False
-            node = node.children[char]
-        return True
+
+def read_words_from_file(file_path):
+    with open(file_path, 'r') as file:
+        word_list = [line.strip() for line in file]
+    return word_list
 
 
-def create_trie(patterns):
-    trie = Trie()
-    for pattern in patterns:
-        trie.add_pattern(pattern)
-    return trie
+def write_result_to_file(result, file_path):
+    with open(file_path, 'w') as file:
+        file.write(str(result))
